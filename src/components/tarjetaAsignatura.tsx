56ba4d2010d5b12a71be0e7112a5c824c45849da
David Cachaza <9005214@alumnos.ufv.es>
2023-02-02 19:21:28 +0100

    optimizada la manera de obtener tiempoTotal y hecho una manera para añadirlo a la base de datos una vez conseguido mas tiempo mas

--- src/components/tarjetaAsignatura.tsx
+++ src/components/tarjetaAsignatura.tsx
@@ -14,11 +14,6 @@ interface Asignatura {
 
 const TarjetaAsignatura: React.FC<Asignatura> = ({ nombre, asignaturaId, tiempoObjetivo, tiempoTotal}) => {
 
-    const { data: sessionData } = useSession();
-    const getTiempos = api.asignaturas.getTiempos.useQuery({ id: sessionData?.user.id, asignaturaId: asignaturaId });
-
-
-    
 
     function pasarHoraOMinutos(tiempo: number) {
         if (tiempo >= 60) {
@@ -31,35 +26,20 @@ const TarjetaAsignatura: React.FC<Asignatura> = ({ nombre, asignaturaId, tiempoO
         }
     }
 
-
-
     const tiempoFinal = pasarHoraOMinutos(tiempoTotal);
     
 
-   
-    
-
-    
-
-
-    
-
-
     return (
         <a className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 col-span-12 sm:col-span-6 md:col-span-3"
-                        onClick={() => Router.push("/a/" + asignaturaId)}
-                        key={asignaturaId}
-                        >
-                            <img className="object-cover w-1/2 h-full rounded-t-lg md:h-auto md:w-2/5 md:rounded-none md:rounded-l-lg" src="https://cdn.discordapp.com/attachments/1061231463405719556/1067003573499609088/cachvza_a_person_alone_walking_on_a_deserted_and_mountainous_pa_3aab462e-773a-414b-8565-547f33842059.png" alt=""></img>
-                            <div className="flex flex-col justify-between p-4 leading-normal">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{nombre}</h5>
-                                {getTiempos.isLoading ? (
-                                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Cargando...</p>
-                                ) : (
-                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{tiempoFinal}</p>
-                                )}
-                            </div>
-                        </a>
+            onClick={() => Router.push("/a/" + asignaturaId)}
+            key={asignaturaId}
+        >
+            <img className="object-cover w-1/2 h-full rounded-t-lg md:h-auto md:w-2/5 md:rounded-none md:rounded-l-lg" src="https://cdn.discordapp.com/attachments/1061231463405719556/1067003573499609088/cachvza_a_person_alone_walking_on_a_deserted_and_mountainous_pa_3aab462e-773a-414b-8565-547f33842059.png" alt=""></img>
+            <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{nombre}</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{tiempoFinal}</p>
+            </div>
+        </a>
     )
 }
 
