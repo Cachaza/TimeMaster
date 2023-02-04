88ab2e4eefdd4fddb4041c8e713569b7709c433a
David Cachaza <9005214@alumnos.ufv.es>
2023-02-04 17:56:18 +0100

    modificado el navbar y el comportamiento del home

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -29,17 +29,9 @@ import Link from "next/link";
       <ul className="flex flex-col mt-4 bg-gray-50 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
         <li>
           <a href="."  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">
-            Home</a>
+          {sessionData ? "Panel" : "Home"}</a>
         </li>
-        {
-          sessionData ? (
-            <li>
-              <Link href="/user" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">            
-                Usuario   
-              </Link>
-              
-            </li>) : null
-        }
+        
         <li>
           <a onClick={sessionData ? () => signOut({callbackUrl: `${window.location.origin}`}) : () => signIn()} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
           {sessionData ? "Sign out" : "Sign in"}</a>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,13 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, getSession } from "next-auth/react";
 import Navbar from "../components/navbar";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
-
-import {useState} from "react";
-import Pomodoro from '../components/pomodoro';
-
-
-
-import { api } from "../utils/api";
 
 const Home: NextPage = () => {
-  const { data: sessionData, status } = useSession();
-  const hello = api.example.nombre.useQuery({ id: sessionData?.user?.id });
-  const [showSettings, setShowSettings] = useState(false);
 
-  if (!sessionData){
+
     return (
       <>
         <Head>
@@ -46,39 +37,28 @@ const Home: NextPage = () => {
         </main>
       </>
     );
-  } else {
-    
-
-    return (
-      <>
-        <Head>
-          <title>Inicio</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Navbar />
-        <main className="-mt-20 container mx-auto flex min-h-screen flex-col items-center justify-center p-4">        
-          <div className="text-4xl text-white">
-            Bienvenido a la prueba
-          </div>
-          {/* 
-          Buen ejemplo para coger info de la base de datos
-          <div className="flex w-full items-center justify-center pt-6 text-2xl text-blue-500">
-            <p>{hello.data ? hello.data.name : "Loading..."}</p>
-          </div>
-          */}
-
-
-          
-          
-
-          
-        </main>
-      </>
-    );
-  }
+  
+  
 };
 
 export default Home;
 
 
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+  const session = await getSession(context);
+  
+  if (session) {
+      return {
+          redirect: {
+              destination: "/user",
+              permanent: false,
+          },
+      };
+  }
 
+  return {
+      props: {
+          session,
+      },
+  };
+}
