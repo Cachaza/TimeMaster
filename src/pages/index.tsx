377308d0f71526445ae8703b61cad61c7334b6de
David Cachaza <9005214@alumnos.ufv.es>
2023-02-08 23:09:33 +0100

    actualizado index del esquema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,10 +26,10 @@ const Home: NextPage = () => {
             <p>{hello.data ? hello.data.name : "Loading..."}</p>
           </div>
           */}
-          <div className="text-xl text-white pb-5">
+          <div className="text-xl bg-blue-700 text-white pb-5">
             Por favor inicia session
           </div>
-          <div className="flex flex-col items-center p-5 bg-white rounded-xl">
+          <div className="flex flex-col items-center p-5 bg-blue rounded-xl">
             <Link href="/login">Inicia session</Link>
           </div>
         </main>
