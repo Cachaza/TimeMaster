4938a74beb08463a0b8de9c3b06ec649da9f67b7
David Cachaza <9005214@alumnos.ufv.es>
2023-02-14 18:44:11 +0100

    made the call of signIn void

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
           </div>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => signIn()}
+            onClick={() => void signIn()}
           >
             Iniciar sesion
           </button>
