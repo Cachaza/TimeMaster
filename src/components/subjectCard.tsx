a01bfaff3558f8ce59c105c7df8bb1d7e25e1d69
David Cachaza <9005214@alumnos.ufv.es>
2023-02-14 18:42:00 +0100

    changed the container from a to Link so i can just use href also renameed a few variables to english

--- src/components/subjectCard.tsx
+++ src/components/subjectCard.tsx
@@ -1,6 +1,6 @@
 import Router from "next/router";
-import { api } from "../utils/api";
-import { useSession } from "next-auth/react";
+import Link from "next/link";
+
 
 
 
@@ -17,12 +17,12 @@ const SubjectCard: React.FC<Asignatura> = ({ nombre, asignaturaId, tiempoObjetiv
 
     function pasarHoraOMinutos(tiempo: number) {
         if (tiempo >= 60) {
-            let horas = tiempo / 60;
-            horas = Math.floor(horas);
-            let minutos = tiempo % 60;
-            return horas + "h " + minutos + "min";
+            let hours = tiempo / 60;
+            hours = Math.floor(hours);
+            const minutes = tiempo % 60;
+            return String(hours) + "h " + String(minutes) + "min";
         } else {
-            return tiempo + " minutos";
+            return String(tiempo) + " minutos";
         }
     }
 
@@ -30,8 +30,8 @@ const SubjectCard: React.FC<Asignatura> = ({ nombre, asignaturaId, tiempoObjetiv
     
 
     return (
-        <a className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 col-span-12 sm:col-span-6 md:col-span-3"
-            onClick={() => Router.push("/a/" + asignaturaId)}
+        <Link className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 col-span-12 sm:col-span-6 md:col-span-3"
+            href={"/a/" + asignaturaId}
             key={asignaturaId}
         >
             <img className="object-cover w-1/2 h-full rounded-t-lg md:h-auto md:w-2/5 md:rounded-none md:rounded-l-lg" src="https://cdn.discordapp.com/attachments/1061231463405719556/1067003573499609088/cachvza_a_person_alone_walking_on_a_deserted_and_mountainous_pa_3aab462e-773a-414b-8565-547f33842059.png" alt=""></img>
@@ -39,7 +39,7 @@ const SubjectCard: React.FC<Asignatura> = ({ nombre, asignaturaId, tiempoObjetiv
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{nombre}</h5>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{tiempoFinal}</p>
             </div>
-        </a>
+        </Link>
     )
 }
 
