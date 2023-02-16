0c147fe8087429254373304c34ba3511c5ad06c2
David Cachaza <9005214@alumnos.ufv.es>
2023-02-16 15:56:12 +0100

    created a modal for singing out and added it to the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,8 @@
 
-import { signIn, signOut, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+
 import Link from "next/link";
+import SingOutModal from "./singOutModal";
 
 
  
@@ -10,7 +11,7 @@ import Link from "next/link";
 
 
   const { data: sessionData } = useSession();
-  const router = useRouter();
+
  
 
 
@@ -30,6 +31,9 @@ import Link from "next/link";
               <li>
                 <Link href={sessionData ? "/user" : "/"}  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">
                   {sessionData ? "Panel" : "Home"}</Link>
+              </li>
+              <li>
+                { sessionData ? <SingOutModal /> : <Link href="/login" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</Link> }
               </li>
             </ul>
           </div>
