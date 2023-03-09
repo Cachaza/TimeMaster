e3478bce295e540a2fa7972b509eb766322ad2b3
David Cachaza <9005214@alumnos.ufv.es>
2023-03-09 09:48:40 +0100

    made it a flex-col so that the logging options are stacked

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -52,22 +52,26 @@ export default function Login() {
       <Navbar />
       <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-28 px-14 text-">
         <h1 className="text-4xl font-bold">Inicio de sesion</h1>
-        <p className="mt-3 text-2xl pb-5">You are not signed in</p>
+        <p className="mt-3 text-2xl pb-5">Elije el proveedor que desees para iniciar sesion</p>
         <div className=" flex flex-col items-center p-5 bg-white rounded-xl">
           
-          <div className="p-2">
-            <button
-            onClick={() => void signIn("discord")}
-            className=" p-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 "
-            >
-            Sign in with Discord<Image src="https://cdn-icons-png.flaticon.com/512/2111/2111370.png" className="inline w-6 h-6 ml-2"  alt="" width={10} height={10}/>
-            </button>
-            <button
-            onClick={() => void signIn("google")}
-            className=" p-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600 "
-            >
-            Sign in with Google<Image src="https://cdn-icons-png.flaticon.com/512/281/281764.png" className="inline w-6 h-6 ml-2" alt="" width={10} height={10}/>
-            </button>
+          <div className="p-2 flex flex-col">
+            <div>
+              <button
+                onClick={() => void signIn("discord")}
+                className=" p-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 "
+              >
+                Sign in with Discord<Image src="https://cdn-icons-png.flaticon.com/512/2111/2111370.png" className="inline w-6 h-6 ml-2"  alt="" width={10} height={10}/>
+              </button>
+            </div>
+            <div className="pt-2">
+              <button
+                onClick={() => void signIn("google")}
+                className="p-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600 "
+              >
+                Sign in with Google<Image src="https://cdn-icons-png.flaticon.com/512/281/281764.png" className="inline w-6 h-6 ml-2" alt="" width={10} height={10}/>
+              </button>
+            </div>
           </div>
         </div>
       </div>
