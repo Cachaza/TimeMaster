63d707195fe9fe890171e470da7c665e01764cce
Cachaza <davidcachazav@gmail.com>
2023-04-23 01:14:43 +0200

    Made volume buttons increase and decrease the volume

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -39,11 +39,28 @@ const Player: React.FC<PlayerProps> = ({ youtube }) => {
   };
 
   const handleVolumeChange = (value: number) => {
+    if (value > 1) {
+      value = 1;
+    } else if (value < 0) {
+      value = 0;
+    }
     const audio = audioRef.current;
     if (audio) {
       audio.volume = value;
     }
     setVolume(value);
+  };
+
+  const increaseVolume = () => {
+    if (volume <= 1) {
+      handleVolumeChange(volume + 0.05);
+    }
+  };
+
+  const decreaseVolume = () => {
+    if (volume >= 0) {
+      handleVolumeChange(volume - 0.05);
+    }
   };
 
   if (!song) {
@@ -91,20 +108,25 @@ const Player: React.FC<PlayerProps> = ({ youtube }) => {
       </button>
 
       <div className="flex">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="h-6 w-6 pt-1 pr-2"
+        <button
+          className="pr-4 pt-0.5 hover:text-gray-500"
+          onClick={decreaseVolume}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M17.25 9.75L19.5 12m0 0l2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6l4.72-4.72a.75.75 0 011.28.531V19.94a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.506-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.395C2.806 8.757 3.63 8.25 4.51 8.25H6.75z"
-          />
-        </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6 pt-1 pr-2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.25 9.75L19.5 12m0 0l2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6l4.72-4.72a.75.75 0 011.28.531V19.94a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.506-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.395C2.806 8.757 3.63 8.25 4.51 8.25H6.75z"
+            />
+          </svg>
+        </button>
 
         <div className="w-40">
           <input
@@ -118,20 +140,25 @@ const Player: React.FC<PlayerProps> = ({ youtube }) => {
             onChange={(e) => handleVolumeChange(parseFloat(e.target.value))}
           />
         </div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="h-6 w-6 pt-1 pl-2"
+        <button
+          className="pr-4 pt-0.5 hover:text-gray-500"
+          onClick={increaseVolume}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
-          />
-        </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6 pt-1 pl-2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
